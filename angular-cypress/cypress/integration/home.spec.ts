0aa7afd74a23672ba922f5837a51ac2e2c9c46a0
Anuroop <anuroopjoy@gmail.com>
2021-10-03 23:56:33 +0530

    home

--- angular-cypress/cypress/integration/home.spec.ts
+++ angular-cypress/cypress/integration/home.spec.ts
@@ -1,49 +1,47 @@
-describe('Home', () => {
-  it('City Actual API', () => {
-    cy.visit('/');
-    cy.url().should('include', '/#/login');
-    cy.get('#username').type('admin').should('have.value', 'admin');
-    cy.get('#pwd').type('admin').should('have.value', 'admin');
-    cy.contains('Submit').click();
+describe('Home Tests', () => {
+  it('Home Test Actual API', () => {
+    cy.visit('/#/home');
     cy.url().should('include', '/#/home');
-    cy.get('p').should('have.length', 2);
-    cy.get('img').should('have.length', 2);
     cy.fixture('cities.json').then(
       (cities: { name: string; image: string; alt: string }[]) => {
+        cy.get('p').should('have.length', cities.length);
+        cy.get('img').should('have.length', cities.length);
         cities.forEach((city, index) => {
-          cy.get(`:nth-child(${index + 1}) > p`).should('have.text', city.name);
+          cy.get(`:nth-child(${index + 1}) > p`)
+            .should('be.visible')
+            .should('have.text', city.name);
           cy.get(`:nth-child(${index + 1}) > img`)
+            .should('be.visible')
             .should('have.attr', 'src', `assets/${city.image}`)
-            .should('have.attr', 'alt', city.alt)
-            .should('be.visible');
+            .should('have.attr', 'alt', city.alt);
         });
       }
     );
   });
-  it('City Mock API', () => {
+  it('Home Test Mock API', () => {
     cy.intercept(
       {
         method: 'GET',
         url: '/cities',
       },
-      { fixture: 'cities.json' }
+      {
+        fixture: 'cities.json',
+      }
     ).as('getCities');
-    cy.visit('/');
-    cy.url().should('include', '/#/login');
-    cy.get('#username').type('admin').should('have.value', 'admin');
-    cy.get('#pwd').type('admin').should('have.value', 'admin');
-    cy.contains('Submit').click();
+    cy.visit('/#/home');
     cy.url().should('include', '/#/home');
-    cy.get('p').should('have.length', 2);
-    cy.get('img').should('have.length', 2);
     cy.fixture('cities.json').then(
       (cities: { name: string; image: string; alt: string }[]) => {
+        cy.get('p').should('have.length', cities.length);
+        cy.get('img').should('have.length', cities.length);
         cities.forEach((city, index) => {
-          cy.get(`:nth-child(${index + 1}) > p`).should('have.text', city.name);
+          cy.get(`:nth-child(${index + 1}) > p`)
+            .should('be.visible')
+            .should('have.text', city.name);
           cy.get(`:nth-child(${index + 1}) > img`)
+            .should('be.visible')
             .should('have.attr', 'src', `assets/${city.image}`)
-            .should('have.attr', 'alt', city.alt)
-            .should('be.visible');
+            .should('have.attr', 'alt', city.alt);
         });
       }
     );
